a5847dea167eae96d820269276c4342e8d03fb03
angelinn <angelin.nedelchev@outlook.com>
2017-11-10 23:04:53 +0200

    Add minimize to tray.

--- src/ApplicationMonitor/ApplicationMonitor.WPF/MainWindow.xaml.cs
+++ src/ApplicationMonitor/ApplicationMonitor.WPF/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,14 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
+using OpenFile = Microsoft.Win32.OpenFileDialog;
 
 namespace ApplicationMonitor.WPF
 {
@@ -28,6 +32,18 @@ namespace ApplicationMonitor.WPF
         {
             InitializeComponent();
             DataContext = MainViewModel;
+
+            NotifyIcon notifyIcon = new NotifyIcon
+            {
+                Visible = true,
+                Icon = new Icon("icon.ico")
+            };
+
+            notifyIcon.DoubleClick += (s, e) =>
+            {
+                Show();
+                WindowState = WindowState.Normal;
+            };
         }
 
         private void OnStartClicked(object sender, RoutedEventArgs e)
@@ -37,11 +53,19 @@ namespace ApplicationMonitor.WPF
 
         private void OnBrowseClicked(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog dialog = new OpenFileDialog();
+            OpenFile dialog = new OpenFile();
             if (dialog.ShowDialog().GetValueOrDefault())
             {
                 MainViewModel.FilePath = dialog.FileName;
             }
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                Hide();
+
+            base.OnStateChanged(e);
         }
     }
 }

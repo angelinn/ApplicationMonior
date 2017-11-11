8be6771514e95829fcfc93b8372dfcb4b575ba1e
angelinn <angelin.nedelchev@outlook.com>
2017-11-11 10:24:15 +0200

    Add ShowWindow DllImport.

--- src/ApplicationMonitor/MonitoringService/ApplicationMonitoring.cs
+++ src/ApplicationMonitor/MonitoringService/ApplicationMonitoring.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +13,7 @@ namespace MonitoringService
     {
         private Process process;
 
-        public void Begin(string path)
+        public void Begin(string path, int refresh = 30000)
         {
             Launch(path);
 
@@ -20,12 +21,25 @@ namespace MonitoringService
             {
                 while (true)
                 {
-                    await Task.Delay(30000);
+                    await Task.Delay(refresh);
 
                     if (process.HasExited)
                         Launch(path);
                 }
             });
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 5;
+
+        public void ShowMonitored(bool show)
+        {
+            if (show)
+                ShowWindow(process.MainWindowHandle, SW_SHOW);
+            else
+                ShowWindow(process.MainWindowHandle, SW_HIDE);
         }
 
         private void Launch(string path)
